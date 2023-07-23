0828b732f71e606c1bdf54941f239e54a7852620
Kos <ne.veshay.nos@gmail.com>
2023-07-23 21:16:43 +0300

    Top list of languages is added

--- the_most_popular_language.rb
+++ the_most_popular_language.rb
@@ -4,8 +4,17 @@ file_path = File.join(".", "the_most_popular_language.json")
 
 languages = JSON.parse(File.read(file_path, encoding: "UTF-8"))
 
-the_most_popular_language = languages.sort_by { |lang| lang[1] }.last
+# Top list of programming languages
+puts "Top list of programming languages"
+puts
+languages.sort_by { |lang| lang[1] }.reverse.each.with_index(1) do |pair, index|
+  # puts index
+  puts "#{index}. #{pair[0]} - #{pair[1]} %"
+end
 
+# The most popular language only
+the_most_popular_language = languages.sort_by { |lang| lang[1] }.reverse.first
 
-puts("The_most_popular_language: #{the_most_popular_language[0]} " +
-     "with #{the_most_popular_language[1]} %")
+puts
+puts("The most popular language: #{the_most_popular_language[0]} " +
+     "- #{the_most_popular_language[1]} %")
